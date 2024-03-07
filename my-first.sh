93035d82cca8a1e12d592ab38c179b88fcde1e95
Kartheek Deekonda <69098367+kartheekdeekonda@users.noreply.github.com>
2024-03-07 19:38:19 +0530

    second commit

--- my-first.sh
+++ my-first.sh
@@ -6,3 +6,11 @@ echo "My first shell learning"
 a=10
 
 echo $a
+
+echo -e "\e[31mThis is first colour\e[0m"
+echo -e "\e[32mThis is first colour\e[0m"
+echo -e "\e[33mThis is first colour\e[0m"
+echo -e "\e[34mThis is first colour\e[0m"
+echo -e "\e[35mThis is first colour\e[0m"
+echo -e "\e[36mThis is first colour\e[0m"
+
